102366ae67bf7d7a199a9532a4c1b7ab8c1e8d49
Prasta Maha <52882935+prastamaha@users.noreply.github.com>
2026-07-20 22:53:50 +0700

    feat: open registration (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
   site: 'https://kcd.cloudnative.id',
   redirects: {
     '/apply-speaker': 'https://sessionize.com/kubernetes-community-days-indonesia-2026/',
+    '/register': 'https://loket.com/event/kcd-indonesia-2026',
   },
   vite: {
     plugins: [tailwindcss()],
